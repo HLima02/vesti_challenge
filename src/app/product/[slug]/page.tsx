0d74b95d4a73eb097cb79a77e4c3cb7fa3cd8684
HLima02 <hiago.liima02@hotmail.com>
2025-05-16 19:09:24 -0300

    Página de produto: resposividade da tela

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,5 +1,3 @@
-//link para requisição do produto completo
-//https://apivesti.vesti.mobi/appmarca/v1/products/company/0ec63cc6-04cb-4400-a0e9-5703eeed20e4/product/jaqueta-perolas-x-22028/showcase?cid=null&reseller_id=null
 import React, {useState, useEffect} from 'react'
 import ProductSlider from '@/app/components/ProductSlider'
 import ProductDetail from '@/app/components/ProductDetail'
