ccc4cee3bd8a12b4dee1f5b277e04cf0784fc9ee
HLima02 <hiago.liima02@hotmail.com>
2025-05-21 11:15:30 -0300

    Página de finalização do pedido

--- src/app/finalize_order/page.tsx
+++ src/app/finalize_order/page.tsx
@@ -1,10 +1,35 @@
+'use client'
 import React from 'react'
 import ProtectedRoute from '../components/ProtectedRouter'
+import { useProductContext } from '@/contexts/ProductContext'
+import CartItem from '../components/CartITem'
+import { toast } from 'react-toastify'
+import { useRouter } from 'next/navigation'
+
+import './style.scss'
+
 export default function FinalizeOrder() {
+  const { cart } = useProductContext()
+  const router = useRouter()
+
+  function handleOrder(){
+    toast.success("Seu pedido foi enviado com sucesso!")
+    router.push("/")
+  }
+  
   return (
     <ProtectedRoute>
-      <div>
-        <h2>Página de finalização do pedido</h2>
+      <div className='finalize_container'>
+        <h2>Resumo do pedido</h2>
+
+        <ul className='order_list'>
+          {cart.map(item => (
+            <CartItem key={item.code} item={item} />
+          ))}
+        </ul>
+        <div className='order_finalize'>
+          <button onClick={handleOrder}>Finalizar pedido</button>
+        </div>
       </div>
     </ProtectedRoute>
   )
